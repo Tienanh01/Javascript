51955e2c76334d895c9caf3448a4ca9d6ab1ca76
Tienanh01 <63101309+Tienanh01@users.noreply.github.com>
2023-07-13 00:10:21 +0700

    start bootstrap

---
+++ startbootstrap/asset/js/main.js
@@ -0,0 +1,8 @@
+
+
+function myfunction(){
+    let menu_item =  document.getElementById("navbar-collages");
+    // menu_item.classList.add("active")
+    menu_item.classList.toggle("active");
+    menu_item.style.transition = "all 2s";
+}
